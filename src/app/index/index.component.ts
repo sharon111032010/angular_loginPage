3ca25a37541f82baea146de7e55b236d497758ad
sharon101152 <sharon5213375@gmail.com>
2024-05-22 20:55:47 +0800

    改了學姊pull的檔案,ngfor 改完

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -8,6 +8,24 @@ import { Router } from '@angular/router';
 })
 export class IndexComponent {
   
+  sectionContainer = [
+    { id: 1, title: 'Getting Start', lessons: ['learson 1', 'learson 2', 'learson 3'] },
+    { id: 2, title: 'Listen', lessons: ['learson 1', 'learson 2', 'learson 3', 'learson 4'] },
+    { id: 3, title: 'Getting Start Again', lessons: ['learson 1', 'learson 2', 'learson 3'] }
+  ];
+
+  sidebarContent = [
+    {
+      title: 'Lorem it',
+      buttons: ['Lorem, ipsum 1', 'Lorem, ipsum 2', 'Lorem, ipsum 3']
+    },
+    {
+      title: 'Lorem it',
+      buttons: ['Lorem, ipsum 1', 'Lorem, ipsum 2']
+    }
+  ];
+  start(){
+  }
   ngOnInit(): void {
   }
   constructor(private router:Router) { } // <-- 注入router:Router
